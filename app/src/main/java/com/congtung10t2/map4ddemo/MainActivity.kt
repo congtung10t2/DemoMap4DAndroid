a2c0d058f3d6413943f93868e188b9aac87d86f5
tung hoang cong <congtung10t2@gmail.com>
2019-06-22 16:46:08 +0700

    Init

---
+++ app/src/main/java/com/congtung10t2/map4ddemo/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.congtung10t2.map4ddemo
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu
+import android.view.MenuItem
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+import vn.map4d.map4dsdk.maps.Map4D
+import vn.map4d.map4dsdk.maps.OnMapReadyCallback
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mapView.getMapAsync(this)
+    }
+    override fun onMapReady(map4D: Map4D?) {
+        map4D?.enable3DMode(false)
+        //TODO
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
